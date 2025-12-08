a4e3e418fe092025262190be89d99333fe61b6ff
Henrik Hagfeldt <henke9606@hotmail.com>
2025-12-08 17:49:12 +0100

    day 6 done

--- day6/solution.py
+++ day6/solution.py
@@ -23,36 +23,32 @@ def read_data2(file_path):
     with open(file_path) as f:
         data = f.readlines() 
         number_list = []
+
+        operators_line = data[len(data) - 1]
         operators = []
+        for i, o in enumerate(operators_line.strip()):
+            if o != " ":
+                operators.append((i, o))
 
-        for i, line in enumerate(data):
-            line = line.strip()
+        for o in range(len(operators)):
+            o_index = operators[o][0]
+            next_o_index = len(data[0]) if o == len(operators) - 1 else operators[o+1][0]
 
-            if i == len(data) - 1:
-                operators = line.split()
-            else:
-                number_row = []
-                found_number = ""
-                print("Line: ", line)
-
-                for c in line:
-                    if c == " ":
-                        if len(found_number) > 0:
-                            print("Found number /" + found_number + "/")
-                            number_row.append(found_number)
-                            found_number = ""
-                            continue
-                    found_number += c
-                number_row.append(found_number)
-
-                
-                number_list.append(number_row)
+            column = []
+            for y in range(len(data) - 1):
+                value = ""
+                for x in range(o_index, next_o_index-1):
+                    value += data[y][x].strip("\n")
+                print("Value: ", value)
+                column.append(value)
+            print("Column: ", column)
+            number_list.append(column)
         
-        return (number_list, operators)
+        return number_list, operators
 
 
 ex_data = read_data2("day6/example.data")
-#real_data = read_data2("day6/real.data")
+real_data = read_data2("day6/real.data")
 
 def part1(problems):
 
@@ -81,30 +77,38 @@ def calculate(total, problem, operator):
 
 
 def part2(problems):
+
+    print("Number list", problems)
+
     number_list = problems[0]
     operators = problems[1]
 
-    print("Number list", number_list)
-    print("operators", operators)
+    totals = 0
 
-    totals = [1 if o == "*" else 0 for o in operators]
+    for c, column in enumerate(number_list):
+        print("Column: ", column)
+        operator = operators[c][1]
 
-    for row_number in range(len(number_list[0])):
+        numbers = []
+        for x in range(len(column[0])-1, -1, -1):
+            print(x)
+            current_number = ""            
+            for y in range(len(column)):
+                current_number += column[y][x]
+            numbers.append(int(current_number))
         
-        for x in range(3):
-            number = ""
-            for col_number in range(len(number_list)):
-                number += number_list[col_number][row_number][x]
+        print("Numbers: ", numbers)
+        print("Operator: ", operator)
+        col_total = 1 if operator == "*" else 0
 
-            #print("Found number ", number)
+        for number in numbers:
+            print(col_total, number, operator)
+            if number != None:
+                col_total = calculate(col_total, number, operator)
+        print("Col total: ", col_total)
+        totals += col_total
+
+    print("Result: ", totals)
 
 
-    for problem_array in number_list:
-        for p, problem in enumerate(problem_array):
-            operator = operators[p]
-            totals[p] = calculate(totals[p], problem, operator)
-
-    print("Totals: ", totals)
-    print("Result: ", str(sum(totals)))
-
-part2(ex_data)
+part2(real_data)
